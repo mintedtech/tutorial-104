ba2c8ff0ef11653720b71a5364dfd702e615a49a
SA <mintedtech@gmail.com>
2019-09-08 14:14:19 -0400

    Part 02 Done

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
+# Use rendered templates: HTML pages combined with Python data
+from flask import render_template
+# As done before, import the app variable from the app package
 from app import app
 
 
 # If a user tries to access either no page (e.g. www.touro.edu)
-# or the index page, then call index()
+# nor the index page, then call index()
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'SA'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Robin'},
+            'body': 'The vacation we took was so nice!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
